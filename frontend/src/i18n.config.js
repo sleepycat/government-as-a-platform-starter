fbfb0d866f15989fcb6c961cb97d62e1568bab26
Mike Williamson <mike.williamson@cds-snc.ca>
2019-11-05 14:26:58 -0500

    Switch to minimal format for translations
    The .po format was problematic for testing because the .po files can't
    be required like json/js files can. The webpack loader works in
    development and production no good answers seem to exist for test.
    Switching to minimal means that json files are always available.

--- frontend/src/i18n.config.js
+++ frontend/src/i18n.config.js
@@ -6,9 +6,19 @@ export const locales = {
 }
 
 export async function activate(locale) {
-  const catalog = await import(
-    /* webpackChunkName: "i18n-[index]" */ `@lingui/loader!./locales/${locale}.po`
-  )
+  let catalog
+  if (process.env.NODE_ENV === 'production') {
+    catalog = await import(
+      /* webpackMode: "lazy", webpackChunkName: "i18n-[index]" */
+      `./locales/${locale}.js`
+    )
+  } else {
+    // for production or test use json:
+    catalog = await import(
+      /* webpackMode: "lazy", webpackChunkName: "i18n-[index]" */
+      `./locales/${locale}.json`
+    )
+  }
 
   i18n.load(locale, catalog)
   i18n.activate(locale)
